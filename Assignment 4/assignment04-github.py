0ee666fcee2a9a35610e0d14391f77bea58caa34
LonanKeane <77697552+Lonabean98@users.noreply.github.com>
2022-11-04 20:55:50 +0100

    Delete Assignment 4 directory

--- Assignment 4/assignment04-github.py
+++
@@ -1,8 +0,0 @@
-from github import Github
-from config import config as cfg
-apikey = cfg["git@github.com:Lonabean98/Assignment-04.git"]
-
-# use your own key
-g = Github(apikey)
-for repo in g.get_user().get_repos():
-    print(repo.name)
